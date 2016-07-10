b488bc0caee537e672f843122c062f57811347a9
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-10 14:53:35 -0400

    modified html to include 3 divs that are initailly hidden

--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key='hjkfjkhadfhjadfshjadshjkdsjhksdjkh'
-
+import random
+random.randrange(0,101)
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+
+@app.route('/')
+def clear():
+	session.clear()
+	return redirect('/')
 app.run(debug=True)
