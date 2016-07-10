fda729b1f13c268ad3e3808766451bac80b9d606
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-10 16:12:40 -0400

    modified server to generate session key as a random number, then hold it in session until cache is cleared

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key='hjkfjkhadfhjadfshjadshjkdsjhksdjkh'
+
 import random
-random.randrange(0,101)
+
 @app.route('/')
 def index():
+	if not session.has_key('number'):
+		session['number'] = random.randint(1,100)
+	print 'Random number is', session['number']
 	return render_template('index.html')
 
 
-@app.route('/')
-def clear():
-	session.clear()
-	return redirect('/')
+# @app.route('/')
+# def clear():
+# 	session.clear()
+# 	return redirect('/')
 app.run(debug=True)
