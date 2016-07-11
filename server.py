a4a52b46ce5b33a78b4b809497289a42e54540ff
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-10 20:47:48 -0400

    trying to figure out a way to input variables to html from python

--- server.py
+++ server.py
@@ -9,18 +9,20 @@ def index():
 	if not session.has_key('number'):
 		session['number'] = random.randint(1,100)
 	print 'Random number is', session['number']
-	return render_template('index.html')
+	number = session['number']
+	return render_template('index.html', number=number)
 
 @app.route('/submit', methods=['POST'])
 def submit():
 	if session['number'] == int(request.form['guess']):
 		print 'Your Guess is Correct'
-
+		showme3 = 'showme'
 	elif int(request.form['guess']) > session['number']:
 		print 'Your Guess is Too High'
-
+		showme2='showme'
 	elif int(request.form['guess']) < session['number']:
 		print 'Your Guess is Too Low'
+		showme1='showme'
 	return redirect('/')
 # @app.route('/')
 # def clear():
